f3a67f95aca3bd375b7517a3483cdc9803fe34fe
Your name <OlatunjiNicholas.com>
2025-08-08 20:12:37 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const day = "Friday"
+
+console.log(day)
+console.log(typeof(day))
+
+const position = 5
+
+console.log(position)
+console.log(typeof(position))
+
+const output = "Today is " + day + ". " +"It is the " + position +"th day of the week" + "." 
+
+console.log(output)
+
